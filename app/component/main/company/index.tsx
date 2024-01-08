0f6cb72b9204fd5e3a320a567cf2d95dc06a1ec2
haku10 <isptry5689@gmail.com>
2024-01-08 21:37:45 +0900

    原型の皮を作成

--- app/component/main/company/index.tsx
+++ app/component/main/company/index.tsx
@@ -1,10 +1,75 @@
+import styles from "./style.module.css";
+
 export const Company = () => {
   return (
     <>
-      <div className="bg-second-black text-white">
+      <div className="">
         <h2 className="text-title text-center">COMPANY</h2>
-        <div>
-          テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        <div className="flex justify-center items-start my-8 md:px-36">
+          {/* 顔写真 */}
+          <div className="flex-1 flex justify-center bg-gray-700 md:w-112 md:h-130">
+            <img
+              src="/face-photo.jpg"
+              alt="face-photo"
+              className="w-48 h-48 object-cover rounded-full"
+            />
+          </div>
+          {/* テキスト */}
+          <div className="flex-1 md:ml-4">
+            テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+          </div>
+        </div>
+        <h3 className="mt-16 text-center">会社概要</h3>
+        <div className={`${styles.tableContainer}`}>
+          <ul className={`${styles.aboutCompanyRow}`}>
+            <li className={styles.itemKey}>
+              <div className="h-12 ml-12 mt-4">社名</div>
+            </li>
+            <li className={styles.itemKey}>
+              <div className="h-12 ml-12 mt-4">設立</div>
+            </li>
+            <li className={styles.itemKey}>
+              <div className="h-12 ml-12 mt-4">代表取締役</div>
+            </li>
+            <li className={styles.itemKey}>
+              <div className="h-12 ml-12 mt-4">住所</div>
+            </li>
+            <li className={styles.itemKey}>
+              <div className="h-12 ml-12 mt-4">連絡先</div>
+            </li>
+            <li className={styles.itemKey}>
+              <div className="h-12 ml-12 mt-4">事業内容</div>
+            </li>
+          </ul>
+          <ul className={`${styles.aboutCompanyRow}`}>
+            <li className={styles.itemValue}>
+              <div className="h-12 mt-4">説明1</div>
+            </li>
+            <li className={styles.itemValue}>
+              <div className="h-12 mt-4">説明3</div>
+            </li>
+            <li className={styles.itemValue}>
+              <div className="h-12 mt-4">説明5</div>
+            </li>
+            <li className={styles.itemValue}>
+              <div className="h-12 mt-4">説明6</div>
+            </li>
+            <li className={styles.itemValue}>
+              <div className="h-12 mt-4">説明7</div>
+            </li>
+            <li className={`${styles.itemValue}`}>
+              <div className="h-12 flex flex-col justify-center items-start">
+                ITコンサルティング
+              </div>
+              <div className="h-12 flex flex-col justify-center items-start">WEB開発</div>
+              <div className="h-12 flex flex-col justify-center items-start">
+                ITコンサルティング
+              </div>
+              <div className="h-12 flex flex-col justify-center items-start">
+                ITコンサルティング
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </>
--- app/component/main/service/index.tsx
+++ app/component/main/service/index.tsx
@@ -1,12 +1,48 @@
 import styles from "./style.module.css";
+import Image from "next/image";
 
 export const Service = () => {
   return (
     <>
-      <div className="bg-second-black text-white">
+      <div className="bg-second-black text-white pb-16">
         <h2 className="text-title text-center">SERVICE</h2>
         <div className={`${styles.service}`}>
           テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+        </div>
+        <div className="mb-105">
+          <div className={`${styles.part} flex justify-center items-center space-x-4`}>
+            <div>Web Development</div>
+            <div>Consulting</div>
+          </div>
+          <div className="flex justify-center items-center space-x-4">
+            {/* ボタンコンテナ */}
+            <div className="flex gap-16">
+              <div className="flex flex-col items-center">
+                <div className="relative w-40 h-40 rounded-full overflow-hidden">
+                  <Image src="/sample-explain.svg" alt="mission" layout="fill" objectFit="cover" />
+                </div>
+                <div className="mt-1">MISSION</div>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="relative w-40 h-40 rounded-full overflow-hidden">
+                  <Image src="/sample-explain.svg" alt="mission" layout="fill" objectFit="cover" />
+                </div>
+                <div className="mt-1">SERVICE</div>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="relative w-40 h-40 rounded-full overflow-hidden">
+                  <Image src="/sample-explain.svg" alt="mission" layout="fill" objectFit="cover" />
+                </div>
+                <div className="mt-1">COMPANY</div>
+              </div>
+              <div className="flex flex-col items-center">
+                <div className="relative w-40 h-40 rounded-full overflow-hidden">
+                  <Image src="/sample-explain.svg" alt="mission" layout="fill" objectFit="cover" />
+                </div>
+                <div className="mt-1">お問い合わせ</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
