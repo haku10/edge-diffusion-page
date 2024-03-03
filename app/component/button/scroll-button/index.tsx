cd9f4dd1138e4251b8b6c7dc507e754fa654c7b7
haku10 <isptry5689@gmail.com>
2024-03-03 23:22:52 +0900

    ボタンの修正とモーションの修正

--- app/component/button/scroll-button/index.tsx
+++ app/component/button/scroll-button/index.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
 
-export const ScrollButton = () => {
+interface ScrollButtonAboveProps {
+  className?: string;
+  onClick?: () => void;
+}
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // スムーズスクロールを指定
+  });
+};
+
+export const ScrollButtonAbove: React.FC<ScrollButtonAboveProps> = () => {
+  return (
+    <button className="fixed bottom-4 right-4 z-30" onClick={scrollToTop}>
+      <Image src="/scroll-button.svg" alt="scroll-image" width={266} height={266} />
+    </button>
+  );
+};
+
+export const ScrollButtonBottom = () => {
   return (
     <button className="fixed bottom-4 right-4 z-30">
-      <Image src="/scroll-button.svg" alt="scroll-image" width={266} height={266} />
+      <Image src="/scroll-button-under.svg" alt="scroll-image" width={266} height={266} />
     </button>
   );
 };
